53feabae670554d3587ba3cb579d31c44e8d5a0f
lisanne gerrits <lisannegerrits@gmail.com>
2017-11-20 13:15:56 +0100

    Added a basic template for all the files

---
+++ dashboard/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Email: </label><input type="text" name="email" placeholder="Email">
+		<br>
+		<label>Wachtwoord: </label>
+		<input type="text" name="password" placeholder="Wachtwoord">
+	</form>
+</body>
+</html>
