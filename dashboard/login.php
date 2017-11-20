e7415edf2cc20809191fc4a72649545bc7180031
lisanne gerrits <lisannegerrits@gmail.com>
2017-11-20 14:31:15 +0100

    Created a login page, not done yet tho

--- dashboard/login.php
+++ dashboard/login.php
@@ -3,13 +3,52 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
 <body>
-	<form method="POST">
-		<label>Email: </label><input type="text" name="email" placeholder="Email">
-		<br>
-		<label>Wachtwoord: </label>
-		<input type="text" name="password" placeholder="Wachtwoord">
-	</form>
+	<div class='container' id="login">	
+		<div class='row'>
+			<div class='col-lg-12'>
+				<img src="https://www.sitebuilderreport.com/assets/facebook-stock-up-446fff24fb11820517c520c4a5a4c032.jpg" class="img-fluid logo" alt="Responsive image">
+			</div>
+		</div>
+
+		<form method="POST">			
+			<div class="form-group row">
+				<label for="inputEmail3" class="col-sm-2 form-control-label">E-mail</label>
+				<div class="col-sm-10">
+					<input type="email" class="form-control" name="email" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="inputPassword3" class="col-sm-2 form-control-label">Wachtwoord</label>
+				<div class="col-sm-10">
+					<input type="password" class="form-control" name="password" placeholder="Wachtwoord">
+				</div>
+			</div>
+			<div class="form-group row">
+				<div class="col-sm-offset-2 col-sm-10">
+					<input type="submit" name="submit" value="Log in" class="btn btn-secondary">
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<?php 
+		if (isset($_POST["submit"])) {
+			//Collecting the email and password inputs
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+
+			//TODO: when there's a database add a check if the person is logged in + session or cookie
+
+			header("Location: index.php"); //Redirecting to index.php
+			exit();
+		}
+	?>
 </body>
 </html>
