a924251d7975e27d17c2e8617db10e282c283750
laserawesome <j.l.ubink@gmail.com>
2017-11-21 21:48:27 +0100

    Nog een update.

--- index.php
+++ index.php
@@ -3,51 +3,45 @@
     <head>
     <meta charset="UTF-8">
     <title>Berichten</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <script>
-        $(function(){$('#myCarousel').carousel();});
-    </script>
     </head>
     
     <body>
-    <a href="#" class="button">Test</a>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <ol>
-                <?php
-                print("<li data-target='#myCarousel' data-slide-to='0' class='active'></li>");
-                print("<li data-target='#myCarousel' data-slide-to='1'></li>");
-                print("<li data-target='#myCarousel' data-slide-to='2'></li>");
 
-                ?>
-            </ol>
-            <?php
-            print("<div class='carousal-inner'>");
-            print("<div class='item active'>");
-
-            print("</div>");
-            print("<div class='item'>");
+        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+            <h1 class="navbar-brand">Berichten</h1>
             
-            print("</div>");
-            print("<div class='item'>");
-            
-            print("</div>");
+            <div class="btn-group" role="group">
+            <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Scherm
+            </button>
+            <div class="dropdown-menu" aria-labelledby="btnGroup">
+            <a class="dropdown-item" href="/index.php">Dia</a>
+            <a class="dropdown-item" href="#">Dia2</a>
+            </div>
 
-            print("</div>");
-            ?>
-            <a class="left carousal-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Vorige</span>
-            </a>
-            <a class="right carousal-control" href="#myCarousal" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Volgende</span>
-            </a>
+            <a class="btn btn-primary" href="/dashboard/login.php">Login</a>
+            </div>
+        </nav>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img class="d-block img-fluid" src="https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="https://images.pexels.com/photos/263189/pexels-photo-263189.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="https://images.pexels.com/photos/210552/pexels-photo-210552.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="Third slide">
+                </div>
+            </div>
         </div>
-    
+
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 </html>
