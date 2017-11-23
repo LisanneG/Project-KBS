80c21a3ec0a7c7c017eddeb40f7fb1edadb0dc2d
lisanne gerrits <lisannegerrits@gmail.com>
2017-11-23 12:10:13 +0100

    Fixed logout

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,6 +7,12 @@ if(!isset($_SESSION["email"])){
 	exit();
 }
 
+if (isset($_POST["logout"])) {
+	session_destroy(); //Removing the login session
+
+	header("Location: login.php"); //Redirecting to login.php
+	exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -124,17 +130,7 @@ if(!isset($_SESSION["email"])){
 	      </div>
 	    </div>
 	  </div>
-	</div>
-
-	<?php
-		if (isset($_POST["logout"])) {
-			session_destroy(); //Removing the login session
-
-			header("Location: login.php"); //Redirecting to login.php
-			exit();
-		}
-
-	?>
+	</div>	
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
