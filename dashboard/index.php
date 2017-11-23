b3e0dadba30117aa3264f2c5bf6f5738bfe5f991
lisanne gerrits <lisannegerrits@gmail.com>
2017-11-23 10:27:30 +0100

    Changed the layout a bit

--- dashboard/index.php
+++ dashboard/index.php
@@ -13,7 +13,47 @@ if(!isset($_SESSION["email"])){
 <head>
 	<meta charset="UTF-8">
 	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">	
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<a class="navbar-brand" href="#">Dotcasting</a>
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+
+	  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    	<ul class="navbar-nav mr-auto">
+	      		<li class="nav-item active">
+	        		<a class="nav-link" href="#">Home</a>
+	      		</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">Nieuwsbericht</a>
+		      	</li>
+		      	<form class="form-inline">
+				    <div class="input-group">
+				      <span class="input-group-addon" id="basic-addon1">ICON</span>
+				      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+				    </div>
+				  </form>	      		      	
+	    	</ul>	    
+	    	<ul class="navbar-nav">
+				<li class="nav-item"><a class="nav-link" href="#">Welkom: <?php echo $_SESSION["email"]; ?></a></li>
+				<li class="nav-item">
+			    	<a class="nav-link" href="#">ICON</a>
+			  	</li>
+			  	<li class="nav-item">
+			    	<button type="button" class="btn">Uitloggen</button>
+			  	</li>
+			</ul>	
+	  	</div>
+	</nav>
+
+	<img src="../img/dotsolutions-logo.png" alt="dotsolutions logo" class="img-fluid">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 </body>
 </html>
--- dashboard/login.php
+++ dashboard/login.php
@@ -9,15 +9,12 @@ include 'framework.php';
 	<meta charset="UTF-8">
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">	
 </head>
 <body>
 	<div class='container' id="login">	
-		<div class='row'>
-			<div class='col-lg-12'>
+		<div class='row justify-content-center'>
+			<div class='col-lg-6'>
 				<img src="https://www.sitebuilderreport.com/assets/facebook-stock-up-446fff24fb11820517c520c4a5a4c032.jpg" class="img-fluid logo" alt="Responsive image">
 			</div>
 		</div>
@@ -41,24 +38,33 @@ include 'framework.php';
 		?>		
 
 		<form method="POST">			
-			<div class="form-group row">
+			<div class="form-group row justify-content-center">
 				<label for="inputEmail3" class="col-sm-2 form-control-label">E-mail</label>
-				<div class="col-sm-10">
+				<div class="col-sm-4">
 					<input type="email" class="form-control" name="email" placeholder="Email">
 				</div>
 			</div>
-			<div class="form-group row">
+			<div class="form-group row justify-content-center">
 				<label for="inputPassword3" class="col-sm-2 form-control-label">Wachtwoord</label>
-				<div class="col-sm-10">
+				<div class="col-sm-4">
 					<input type="password" class="form-control" name="password" placeholder="Wachtwoord">
 				</div>
 			</div>
 			<div class="form-group row">
-				<div class="col-sm-offset-2 col-sm-10">
+				<div class="col-sm-2 offset-sm-6">
+					<button class="btn btn-secondary">Wachtwoord vergeten</button>
+				</div>
+				<div class="col-sm-1">
 					<input type="submit" name="submit" value="Log in" class="btn btn-secondary">
 				</div>
 			</div>
 		</form>
 	</div>	
+
+	<img src="../img/dotsolutions-logo.png" alt="dotsolutions logo" class="img-fluid dotsolutions_logo">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
