e5234de8bd137c5499b66cd5b3fe497e7314eefd
laserawesome <j.l.ubink@gmail.com>
2017-11-29 14:53:26 +0100

    Made scroll script. Currently goes into a potentail pc/browser crashing loop as nothing slows it down due to it not detecting any messages with the right sort of ID thus making it just rapidly endlessly skip and do stuff over and over again.

--- js/indexscript.js
+++ js/indexscript.js
@@ -11,17 +11,62 @@ function updateClock(){
 $(document).ready(function(){
 
     setInterval('updateClock()', 1000);
-    scroll($('html, #container'), 5000);
-
+    scroll($('html, .container'), 10000);
+    //MessageScroll();
     //LoadWeather("Zwolle");
 });
 
 
 
 function scroll(element, speed) {
-    element.animate({ scrollTop: $("html, #container").height() }, speed, function() {
+    element.animate({ scrollTop: $("html, .container").height() }, speed, function() {
         $(this).animate({ scrollTop: 0 }, speed, scroll(element, speed), 5000);
     });
+}
+
+
+function MessageScroll(){
+    var Listlength = $("ul.list-unstyle.mw-50 > li").length;
+    for(i = 0; i < Listlength; i++){
+        ListScroll(i);
+    }
+    for(i = Listlength; i != 0; i--){
+        ListScroll(i);
+    }
+    $("html, body").animate({ scrollTop: $("#weather").height()}, 1000);
+    setTimeout(function(){ $("html, body").animate({ scrollTop: 0}, 1000)}, 10000);
+
+    
+    MessageScroll();
+}
+
+function ListScroll(i){
+    var listitem = $("li.media.mb-5.mt-5.border.border-dark").eq(i);
+        if($(listitem).attr("[id$='-video']")){
+            $("html, body").animate({ scrollTop: $(listitem).height()});
+            var video = $(listitem).find("video");            
+            video.autoplay = false;
+            video.muted = true;
+            var vidlength = (video.duration) * 1000;
+            video.play();
+            setTimeout(function(){}, (vidlength + 10000));
+        }
+        else if($(listitem).attr("[id$='-message']")){
+            $("html, body").animate({ scrollTop: $(listitem).height()});
+            setTimeout(function(){}, 10000);
+        }
+        else if($(listitem).attr("[id$='-birthdayimg']")){
+            $("html, body").animate({ scrollTop: $(listitem).height()});
+            setTimeout(function(){}, 10000);
+        }
+        else if($(listitem).attr("[id$='-birthdaynoimg']")){
+            $("html, body").animate({ scrollTop: $(listitem).height()});
+            setTimeout(function(){}, 10000);
+        }
+        else if($(listitem).attr("[id$='-messageimg']")){
+            $("html, body").animate({ scrollTop: $(listitem).height()});
+            setTimeout(function(){}, 10000);
+        }
 }
 
 
