82251c0d44c9a17fc11d7541a9650e758e0d943f
MohamedEssabani <m-essabani@hotmail.com>
2017-12-03 23:39:26 +0100

    Finished almost all work on adding news articles Still have to add the functionality of converting pdf files to jpegs

--- dashboard/news/news_send.php
+++ dashboard/news/news_send.php
@@ -4,24 +4,35 @@ if (isset($_POST["submit"])) {
 	$stmt->execute();
 	$locations = $stmt->fetchAll();
 	
-	$checkLocation = array();
-	foreach ($locations as $location) {
-		$locationId = $location["location_id"];
-		//var_dump($_POST[$locationName]);
-		if (isset($_POST[$locationId])) {
-				//$checkLocation = $checkLocation.$_POST[$locationName];
-				var_dump($_POST[$locationId]);
-		}
+	$news_title = $_POST["title"];
+	$categoryId = $_POST["category"];
+	$fileId = $lastInsertedFileId[0];
+	$displayFrom = $_POST["dateFrom"];
+	$displayTill = $_POST["dateTill"];
+	if (isset($_POST["priority"])) {
+		$priority = 1;
+	} else {
+		$priority = 0;
 	}
+	$description = $_POST["description"]; //should be null when video is being uploaded
 	
-	foreach ($checkLocation as $printLocation) {
-		print($printLocation);
+	
+	// inserting newsarticle into db
+	$stmt = $conn->prepare("INSERT INTO news_article (title, category_id, file_id, display_from, display_till, priority, description) 
+	VALUES ('".$news_title."','".$categoryId."','".$fileId."','".$displayFrom."','".$displayTill."','".$priority."','".$description."')");
+	$stmt->execute();
+	$lastInsertedNewsId = $conn->lastInsertId();
+	
+	// inserting relations between newsarticle and locations to display
+	foreach ($_POST["location"] as $v) {
+		$stmt = $conn->prepare("INSERT INTO news_article_has_location (news_article_id, location_id) 
+		VALUES ('".$lastInsertedNewsId."','".$v."')");
+		$stmt->execute();
+		$finished = $conn->lastInsertId();
 	}
-	
-	$stmt = $conn->prepare("INSERT INTO news_article (title, category_id, file_id, location_id, display_from, display_till, priority, description) 
-	VALUES (?,?,?,?,?,?,?,?)");
-	$stmt->execute(array());
-	
-	
+	// making sure everything worked out correctly
+	if (isset($finished)){
+		$success = 1;
+	}
 }
 ?>
