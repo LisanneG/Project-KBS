549b18ce7ae28d805d65e1589e056723a9c815bd
MohamedEssabani <m-essabani@hotmail.com>
2017-12-18 10:55:14 +0100

    update

--- db/dotcasting.sql
+++ db/dotcasting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2017 at 10:49 AM
+-- Generation Time: Dec 18, 2017 at 10:54 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
