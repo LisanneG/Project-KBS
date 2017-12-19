1f8978b3c351f42a23ba517cb6b74972a9c2721f
Willem <Willem@Willem-PC>
2017-12-19 12:21:36 +0100

    Themapagina JW

--- dashboard/Thema/index.php
+++ dashboard/Thema/index.php
@@ -90,7 +90,8 @@ if (isset($_POST["logout"])) {
                                 <tr class="font-bold-weight">
                                     <th>#</th>
                                     <th>Naam</th>
-                                    <th>Foto</th>
+                                    <th>Afbeelding</th>
+                                    <th>Wijzigen</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -126,14 +127,14 @@ if (isset($_POST["logout"])) {
                     <div class="modal-body">
                         <form>
                             <div class="form-group">
-                                <label for="newthemename">Naam</label>
+                                <!--<label for="newthemename">Naam</label>-->
                                 <input type="text" class="form-control" id="newthemename" placeholder="Naam">
                             </div>
 
 
                         </form>
 
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Afbeelding toevoegen</button>
                         </form>
 
                     </div>
@@ -144,6 +145,40 @@ if (isset($_POST["logout"])) {
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="bijwerken" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addmenu">Bijwerken</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <!--<label for="newthemename">Naam</label>-->
+                                <input type="text" class="form-control" id="newthemename" placeholder="Bijwerken">
+                            </div>
+
+
+                        </form>
+
+                        <button type="submit" class="btn btn-primary">Afbeelding toevoegen</button>
+                        </form>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" method="post" type="submit">Opslaan</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuleren</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
 
         <div class="modal fade" id="verwijderen" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
