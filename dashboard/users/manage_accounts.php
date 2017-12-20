3bfc1c203720a77e8a6303c34f39c41ff1345389
LarsAssen <juniorassen@hotmail.com>
2017-12-20 11:56:10 +0100

    accounts managen gemaakt

--- dashboard/users/manage_accounts.php
+++ dashboard/users/manage_accounts.php
@@ -26,42 +26,150 @@ if (isset($_POST["logout"])) {
 </head>
 <body>
 <?php include '../include/navbar.php'; ?>
+
 	
+	<?php 
+		
+	
+	
+	//het updaten van medewerkers:
+	$sql = ("UPDATE user SET
+		first_name=?, 
+		insertion=?, 
+		last_name=?, 
+		birthday=?, 
+		email=?, 
+		password=?, 
+		admin=?, 
+		location=?
+		WHERE user_id=?"
+		);
+	if(isset($_POST["Updaten"]))	{
+		$user_id = $_POST["user_id"];
+		$voornaam = $_POST["voornaam"];
+		$tussenvoegsel = $_POST["tussenvoegsel"];
+		$achternaam = $_POST["achternaam"];
+		$verjaardag = $_POST["verjaardag"];
+		$email = $_POST["email"];
+		$wachtwoord = $_POST["wachtwoord"];
+		$locatie = $_POST["locatie"];
+			if (isset($_POST["admin"])){
+		$admin = 1;
+	} else {
+		$admin = 0;
+	}
+
+		
+		$stmt = GetDatabaseConnection()->prepare($sql);
+		if ($stmt->execute(array($voornaam, $tussenvoegsel, $achternaam, $verjaardag, $email, $wachtwoord, $admin, $locatie, $user_id))){
+			print("<div class=\"alert alert-success\"role=\"alert\">Medewerker succesvol bewerkt</div>");
+		}
+	}
+	
+	$user_id = "";
+	$naam ="";
+	
+	try{
+		// Setting up the info for the database
+		$host = "localhost";
+		$username = "root";
+		$password = "";
+		$db = "dotcasting";
+
+		// Trying to get a connection
+		$conn = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+	}
+	catch(PDOException $ex){
+		// If for some reason the connection can't be made, it will give an error and stop with everything else
+		die("<div class='alert alert-danger' role='alert'><strong>Error:</strong> kan geen verbinding maken</div>");
+	}
+	
+//het toevoegen van medewerkers
+
+if (isset($_POST["submit"])){
+	$required = array("Voornaam","Achternaam","Email","Wachtwoord","Geboortedatum", "Locatie");
+	$error = FALSE;
+	foreach($required as $field) {
+	if(empty($_POST[$field])){
+		header("location: add_user.php");
+	exit();
+		print("<div class=\"alert alert-danger\"role=\"alert\">Alle velden moeten ingevuld worden</div>");
+		break;
+	}else{
+	$voornaam = $_POST["Voornaam"];
+	$tussenvoegsel = $_POST["Tussenvoegsel"];
+	$achternaam = $_POST["Achternaam"];
+	$geboortedatum = $_POST["Geboortedatum"];
+	$email = $_POST["Email"];
+	$wachtwoord = $_POST["Wachtwoord"];
+	$locatie =  $_POST["Locatie"];
+	
+	if (isset($_POST["Admin"])){
+		$admin = 1;
+	} else {
+		$admin = 0;
+	}
+	$sql = "INSERT INTO user(first_name, insertion, last_name, birthday, email, password, admin, location) VALUES (?,?,?,?,?,?,?,?)";
+	$stmt = $conn->prepare($sql);
+	if($stmt->execute(array($voornaam, $tussenvoegsel, $achternaam, $geboortedatum, $email, $wachtwoord, $admin, $locatie))){
+		print("<div class=\"alert alert-success\"role=\"alert\">Medewerker succesvol toegevoegd</div>");
+		break;
+	}
+}
+}
+}
+//het verwijderen van medewerkers
+if(isset($_GET["user_id1"]) && $_GET["user_id1"] != ""){
+	$nummer = $_GET["user_id1"];
+	
+	if (isset($_GET["user_id1"])) {
+	$sql = "DELETE FROM user WHERE user_id =?";
+	$stmt = GetDatabaseConnection()->prepare($sql);
+	$stmt->execute(array($_GET["user_id1"]));
+	}
+}
+?>
+
 	<h1>Medewerkers</h1>
 	<?php 
 		$stringBuilder = "SELECT * FROM user";
 		$query = GetDatabaseConnection()->prepare($stringBuilder);
 		$query->execute();
 		?>
+	<div class="container-fluid" style="border:1px solid #cecece;">
+                <div class="row">
+	<table class= "table table-bordered table-hover">
+	<div class="table-responsive">
 
-	<table class= "table table-bordered table-hover table-striped">
 	<?php
 	
 	echo "<tr><th>User id</th><th>first name</th><th>tussenvoegsel</th><th>achternaam</th><th>geboortedatum</th><th>email</th><th>wachtwoord</th><th>admin</th><th>locatie id</th><th>Update</th><th>Delete</th></tr>";
-	while($row = $query->fetch()){
+	while($user = $query->fetch()){
 		echo("<tr>");
 		echo("
-		<td>".$row['user_id'] ."</td>
-		<td>".$row['first_name'] ."</td>
-		<td>".$row['insertion'] ."</td>
-		<td>".$row['last_name'] ."</td>
-		<td>".$row['birthday'] ."</td>
-		<td>".$row['email'] ."</td>
-		<td>".$row['password'] ."</td>
-		<td>".$row['admin'] ."</td>
-		<td>".$row['location'] ."</td>
-		<td><a href='#'>update</a></td>
-		<td><a href='#'>delete</a></td>
+		<td>".$user['user_id'] ."</td>
+		<td>".$user['first_name'] ."</td>
+		<td>".$user['insertion'] ."</td>
+		<td>".$user['last_name'] ."</td>
+		<td>".$user['birthday'] ."</td>
+		<td>".$user['email'] ."</td>
+		<td>".$user['password'] ."</td>
+		<td>".$user['admin'] ."</td>
+		<td>".$user['location'] ."</td>
+		<td><a href='../users/update_user.php?user_id=". $user["user_id"]."' class='btn btn-info btn-md'>update</a></td>
+		<td><a href='../users/manage_accounts.php?user_id1=". $user["user_id"]."' class='btn btn-info btn-md' name='delete'>delete</a></td>
 		");
 		echo("</tr>");
 	}
 	echo"</table>";
+
 	?>
 	        <a href="../users/add_user.php" class="btn btn-info btn-md">
-          <span class="glyphicon glyphicon-plus"></span> Account toevoegen 
+          <span class="fa fa-plus"></span> Account toevoegen 
         </a>
-	
-
+	</div>
+	</div>
+	</div>
 <?php include '../include/footer.php'; ?>
 </body>
 	
---
+++ dashboard/users/update_user.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+include '../include/framework.php';
+
+if(!isset($_SESSION["email"])){
+	header("Location: login.php"); //Redirecting to login.php
+	exit();
+}
+
+if (isset($_POST["logout"])) {
+	session_destroy(); //Removing the login session
+
+	header("Location: login.php"); //Redirecting to login.php
+	exit();
+}
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Dashboard | Account Updaten</title>
+	<?php include '../include/css.php'; ?>
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">	
+</head>
+<body>
+<?php include '../include/navbar.php'; ?>
+<div class="container">
+	<h1>Account Updaten</h1>
+	
+		<?php 
+		
+	$user_id="";
+	$voornaam="";
+	$tussenvoegsel="";
+	$achternaam="";
+	$verjaardag="";
+	$email="";
+	$wachtwoord="";
+	$locatie="";
+	$admin=0;
+	
+	if(isset($_GET["user_id"]) && $_GET["user_id"] != ""){
+	$user_id = $_GET["user_id"];
+	
+	
+	$sql = ("UPDATE user SET
+		first_name=?, 
+		insertion=?, 
+		last_name=?, 
+		birthday=?, 
+		email=?, 
+		password=?, 
+		admin=?, 
+		location=?
+		WHERE user_id=?"
+		);
+	
+	if(isset($_POST["Updaten"]))	{
+		$stmt = GetDatabaseConnection()->prepare($sql);
+		
+		if ($stmt->execute(array($_POST["voornaam"], $_POST["tussenvoegsel"], $_POST["achternaam"], $_POST["verjaardag"], $_POST["email"], $_POST["wachtwoord"], $_POST["admin"], $_POST["locatie"], $user_id))){
+			print("<div class=\"alert alert-success\"role=\"alert\">Medewerker succesvol bewerkt</div>");
+		}
+	}
+
+		
+		$string = ("SELECT * FROM user WHERE user_id=" . $user_id);
+		$query = GetDatabaseConnection()->prepare($string);
+		$query->execute();
+		if ($row = $query->fetch()) {
+			$voornaam = $row["first_name"];
+			$tussenvoegsel = $row["insertion"];
+			$achternaam = $row["last_name"];
+			$geboortedatum = $row["birthday"];
+			$email = $row["email"];
+			$wachtwoord = $row["password"];
+			$admin = $row["admin"];
+			$locatie = $row["location"];
+		}
+			}
+		?>
+
+		
+	 <form class='form-horizontal' action='manage_accounts.php' method='post'>
+	 	<div class='form-group'>
+	 	<div class='row'>
+	 		<div class='col'>
+	 			<input type='text' class='form-control' name='voornaam' value="<?= $voornaam ?>">
+	 		</div>
+	 		<div class='col'>
+	 			<input type='text' class='form-control' placeholder='tussenvoegsel' name='tussenvoegsel'value="<?= $tussenvoegsel ?>">
+	 		</div>
+	 		<div class='col'>
+	 			<input type='text' class='form-control' placeholder='Achternaam' name='achternaam'value="<?= $achternaam ?>">
+	 		</div>
+	 	</div>
+	 	</div>
+	 	<div class='form-group'>
+	 		<div class='col'>
+	 			<input type='date' class='form-control' placeholder='verjaardag' name='verjaardag'value="<?= $verjaardag ?>">
+	 		</div>
+	 	</div>
+	 	<div class='form-group'>
+	 		<div class='col'>
+	 			<input type='text' class='form-control' placeholder='Email' name='email'value="<?= $email ?>">
+	 		</div>
+	 	</div>
+	 	<div class='form-group'>
+	 		<div class='col'>
+	 			<input type='password' class='form-control' Placeholder='Wachtwoord' name='wachtwoord'value="<?= $wachtwoord ?>">
+	 		</div>
+	 	</div>
+	 	<div class='form-group'>
+	 			<lable class='control-label col-sm-2'>Admin:</lable>
+	 		<div class='col'>
+	 			<input type='checkbox' name='admin'value="<?= $admin ?>">
+	 		</div>
+	 	</div>
+	 	<div class='form-group'>
+	 		<div class='col'>
+	 			<Select class='custom-select' placeholder='Locatie' name='locatie'>
+	 			<option selected>Kies locatie</option>
+	 			<option value='1'>Zwolle</option>
+	 			<option value='2'>Meppel</option>
+	 			<option value='3'>Groningen</option>
+	 			</select>
+	 	</div>
+	 	</br>
+		<input type='hidden' name='user_id' value="<?= $user_id ?>">
+		
+	 	<div class='form-group'>
+	 		<div class='col'>
+	 			<button type='submit' class='btn btn-primary' name='Updaten'>Updaten</button>
+	 		</div>
+	 	</div>
+	 </form>
+	 </div>
+
+
+<?php
+
+include '../include/footer.php';
+?>
+</body>
+	
+</html>
