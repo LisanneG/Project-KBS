d37a428306c956462de3c967147759a46f8a36af
Frank van de Wetering <frankvdw95@gmail.com>
2017-12-21 13:15:00 +0100

    update van de locatie pagina

--- dashboard/location/location_add.php
+++ dashboard/location/location_add.php
@@ -14,6 +14,7 @@ if (isset($_POST["logout"])) {
 	exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,31 +24,40 @@ if (isset($_POST["logout"])) {
 </head>
 <body>
         <?php include '../include/navbar.php'; ?>
-        <!-- Navigational tabs -->
+    
+<!-- Navigational tabs -->
         <section id="dashboard-content" class="container-fluid">
             <h1> Locaties </h1>
-            <nav class="nav nav-tabs" id="locatietab" role="tablist">
-			<a class="nav-item nav-link active" id="nav-toevoegen" data-toggle="tab" href="#nav-toevoegen" role="tab" aria-controls="nav-toevoegen" aria-selected="true">Toevoegen</a>
-			<a class="nav-item nav-link" id="nav-wijzigen" data-toggle="tab" href="#nav-wijzigen" role="tab" aria-controls="nav-wijzigen" aria-selected="false">Wijzigen</a>
+		<nav class="nav nav-tabs" id="loctab" role="tablist">
+			<?php if(CheckIfUserHasRight($_SESSION["admin"], "Aanmaken locatie", $_SESSION["user_id"])){ ?>
+				<a class="nav-item nav-link active" id="nav-add" data-toggle="tab" href="#nav-toevoegen" role="tab" aria-controls="nav-toevoegen" aria-selected="true">Toevoegen</a>
+			<?php } ?>
+			<?php if(CheckIfUserHasRight($_SESSION["admin"], "Bewerken locatie", $_SESSION["user_id"])){ ?>
+				<a class="nav-item nav-link" id="nav-wijzigen-tab" data-toggle="tab" href="#nav-wijzigen" role="tab" aria-controls="nav-wijzigen" aria-selected="false">Wijzigen</a>
+			<?php } ?>
 		</nav>
-            
-        <!-- content of the tab 1 -->
+<!-- content of the tabs -->
+<div class="tab-content" id="nav-tabContent">
+             
+ <!-- content of the tab 1 -->
+        <div class="tab-pane fade show active" id="nav-toevoegen" role="tabpanel" aria-labelledby="nav-toevoegen-tab">
  	<h3>Locatie toevoegen</h3>
+        <form method="post" action="location_add.php">
         	<div class="form-group">
                     <div class="col">
                             <input type="text" class="form-control" placeholder="Plaats" name="Plaats">
                     </div>
 		</div>	
-            <div class="form-group">
-		<div class="row">
+                <div class="form-group">
+                    <div class="row">
 			<div class="col">
 				<input type="text" class="form-control" placeholder="Adres" name="Adres">
 			</div>
 			<div class="col">
 				<input type="text" class="form-control" placeholder="Postcode" name="Postcode">
 			</div>
-		</div>
-            </div>
+                    </div>
+                </div>
 		<div class="form-group">
                     <div class="col">
                             <input type="text" class="form-control" placeholder="Hoofdnummer" name="Hoofdnummer">
@@ -63,15 +73,70 @@ if (isset($_POST["logout"])) {
                             <input type="submit" class="btn btn-primary" name="submit" value="Toevoegen">
 			</div>
                 </div>
+        </form>
+        </div>
+ 
+<!-- content of the tab 2 -->
+        		<div class="tab-pane fade" id="nav-wijzigen" role="tabpanel" aria-labelledby="nav-wijzigen">
+				<div class="row">					
+					<div class="col-md-12">
+						<table class="table">
+							<thead>
+								<tr>								
+									<th>Locatie ID</th>
+									<th>Plaats</th>
+									<th>Adres</th>
+                                                                        <th>Postcode</th>
+                                                                        <th>Hoofdnummer</th>
+                                                                        <th>Intern nummer</th>
+                                                                        <th>Layout ID</th>
+                                                                        <th>Thema ID</th>
+                                                                        <th>Update</th>
+                                                                        <th>Verwijderen</th>
+								</tr>
+                                                                <tr>
+                                                                    	<?php
+                                                                            $stringBuilder = "SELECT * FROM location";
+                                                                            $query = GetDatabaseConnection()->prepare($stringBuilder);
+                                                                            $query->execute();
+	
+                                                                            while($location = $query->fetch())
+                                                                                    {
+                                                                                    echo("<tr>");
+                                                                                    echo("
+                                                                                        <td>".$location['location_id'] ."</td>
+                                                                                        <td>".$location['name'] ."</td>
+                                                                                        <td>".$location['address'] ."</td>
+                                                                                        <td>".$location['postal_code'] ."</td>
+                                                                                        <td>".$location['main_number'] ."</td>
+                                                                                        <td>".$location['intern_number'] ."</td>
+                                                                                        <td>".$location['layout_id'] ."</td>
+                                                                                        <td>".$location['theme_id'] ."</td>
+                                                                                        <td><a href='../location/location_add.php?user_id=". $location["location_id"]."' class='btn btn-info btn-md'>Update</a></td>
+                                                                                        <td><a href='../location/location_add.php?user_id1=". $location["location_id"]."' class='btn btn-info btn-md' name='delete'>Verwijderen</a></td>
+                                                                                    ");
+                                                                                    echo("</tr>");
+                                                                                    }
+                                                                            echo"</table>";
+                                                                        ?>
+                                                                </tr>
+							</thead>
+							<tbody id="location-list">
+							</tbody>
+						</table>
+					</div>					
+				</div>
+			</div>
+         </div>
 	</section> 
-        <!-- content of the tab 1 -->
+
         
 <?php 
 
 if (isset($_POST["submit"])){
-	$sql = "INSERT INTO location (name, address, postal_code, main_number, intern_number) VALUES (?,?,?,?)";
+	$sql = "INSERT INTO location (layout_id, name, address, postal_code, main_number) VALUES (?,?,?,?,?)";
 	$stmt = GetDatabaseConnection()->prepare($sql);
-	$stmt->execute(array($_POST["Plaats"], $_POST["Adres"], $_POST["Postcode"], $_POST["Hoofdnummer"]));
+	$stmt->execute(array("1", $_POST["Plaats"], $_POST["Adres"], $_POST["Postcode"], $_POST["Hoofdnummer"]));
  
 }
 
